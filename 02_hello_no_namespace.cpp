7a12a3077659bc212dde410c1e036b08035a4153
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-06 13:16:10 +0530

    2

---
+++ 02_hello_no_namespace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* If you forget 'using namespace std;' during exams or in code use this method */
+
+int main()
+{
+    std::cout << "hello, world!" << std::endl;
+
+    return 0;
+}
