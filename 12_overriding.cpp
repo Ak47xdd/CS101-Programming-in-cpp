91b860478121612742227f212e01d92be49139db
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-07 16:14:13 +0530

    12 - changes

--- 12_overriding.cpp
+++ 12_overriding.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class A // base class. parent
+class A // base class, parent
 {
 public:
-    virtual void func()
+    virtual void func() // define any function that needs to be overriden with the 'virtual' keyword
     {
         cout << "virtual func";
     }
 };
 
-class B : public A // child class
+class B : public A // subclass, child, ' : access_specifier Class_name' is a must since the class is inherited from base class
 {
 public:
-    void func()
+    void func() // function overriden here with new code, use the same return_type and func_name
     {
         cout << "overriden func";
     }
@@ -21,11 +21,14 @@ public:
 
 int main()
 {
-    A *a1;
-    B b1;
+    A *a1; // parent class called with a pointer to its object (Parent *parent_obj_name;)
+    B b1;  // child class called with its object (Child child_obj_name;)
 
-    a1 = &b1;
-    a1->func();
+    a1 = &b1; // parent pointer object assigned to its dereferenced child object (parent_obj_name = & child_obj_name;)
+
+    a1->func(); // calling the function in child with reference to its parent pointer object (parent_obj_name -> func_name();)
+    cout << endl;
+    b1.func(); // this also works!
 
     return 0;
 }
