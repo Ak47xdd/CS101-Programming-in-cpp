c0550d18663a64c1638db0c29780dfd14b7c512c
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-08 12:10:55 +0530

    17

---
+++ 17_single_inheri.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class A // parent
+{
+public:
+    int x;
+};
+
+class B : public A // child, class A is inherited by class B ( : access_specifier Class_name_of_inheriting{})
+{
+public:
+    string name;
+};
+
+int main()
+{
+    B b1; // call the inherited class (B)s
+
+    return 0;
+}
