cd0e6502001579df85a2ebdaa082971956d9ef01
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-09 10:43:21 +0530

    19 - changes

---
+++ 19_muti.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class A // parent
+{
+public:
+    int salary;
+};
+
+class B : public A // child, inherited from class A
+{
+public:
+    string name;
+};
+
+class C : public A, public B // child, inheriting from A and B
+{
+public:
+    string game;
+};
+
+class D : public A, public B, public C // child, inheriting from A, B and C
+{
+public:
+    int adi_cout;
+};
+
+int main()
+{
+    D d1; // call the class that has inherited from all classes, class D
+          //                        A
+          //                        |
+          //                      B,C,D
+    return 0;
+}
