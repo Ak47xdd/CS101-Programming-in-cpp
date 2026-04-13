a7780ec4a36b413355879072bd2c4e3b3413d6e9
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-13 11:40:09 +0530

    17 - fix

--- 17_single_inheri.cpp
+++ 17_single_inheri.cpp
@@ -4,20 +4,37 @@ using namespace std;
 class A // parent
 {
 public:
-    int x;
+    int age;
+    string name;
+    A(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
 };
 
 class B : public A // child, class A is inherited by class B ( : access_specifier Class_name_of_inheriting{})
 {
 public:
-    string name;
+    string likes;
+    int fav_no;
+    B(string name, int age, string likes, int fav_no) : A(name, age)
+    {
+        this->likes = likes;
+        this->fav_no = fav_no;
+    }
 };
 
 int main()
 {
-    B b1; // call the inherited class (B)s
-          //            A
-          //            |
-          //            B (Call this class!)
+    B b1("akshay", 20, "football", 12);
+
+    cout << "Name: " << b1.name << endl;
+    cout << "Age: " << b1.age << endl;
+    cout << "Likes: " << b1.likes << endl;
+    cout << "Fav no: " << b1.fav_no << endl;
+
+    // Demonstrates single inheritance: B inherits age and name from A
+
     return 0;
 }
