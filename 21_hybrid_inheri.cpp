b0674bd6c58895adc55ddf6043a9d1db3d67f8a9
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-13 11:40:18 +0530

    21

---
+++ 21_hybrid_inheri.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int x;
+};
+
+class B : public A
+{
+public:
+    int y;
+};
+
+class C : public A
+{
+public:
+    int z;
+};
+
+class D : public B
+{
+public:
+    string a;
+};
+
+class E : public C
+{
+public:
+    string b;
+};
+
+class F : public E
+{
+public:
+    string c;
+};
