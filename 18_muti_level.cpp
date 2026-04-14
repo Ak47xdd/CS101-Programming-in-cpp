5f0d9729423ba8ea7306cacd139e0657aa6b47a8
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-14 11:35:34 +0530

    18

--- 18_muti_level.cpp
+++ 18_muti_level.cpp
@@ -5,28 +5,43 @@ class A // parent
 {
 public:
     int x;
+    A(int x)
+    {
+        this->x = x;
+    }
 };
 
 class B : public A // child, inherited from A
 {
 public:
     string name;
+    B(int x, string name) : A(x)
+    {
+        this->x = x;
+        this->name = name;
+    }
 };
 
 class C : public B // child, inherited from B
 {
 public:
     int a;
+    C(int x, string name, int a) : B(x, name)
+    {
+        this->x = x;
+        this->name = name;
+        this->a = a;
+    }
 };
 
 int main()
 {
-    C c1; // Call the lowest in the inheritence tree (C)
-          //                A
-          //                |
-          //                B
-          //                |
-          //                C
+    C c1(10, "akshay", 1); // Call the lowest in the inheritence tree (C)
+                           //                A
+                           //                |
+                           //                B
+                           //                |
+                           //                C
 
     return 0;
 }
