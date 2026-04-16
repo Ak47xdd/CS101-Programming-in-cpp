b5a891d23f729bdcac6833e82d8443217e5f053d
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-04-16 11:52:17 +0530

    25 - changes

--- 25_friend.cpp
+++ 25_friend.cpp
@@ -6,10 +6,10 @@ class A
 private:
     int x;
 
-    friend void display(A obj);
+    friend void display(A obj); // freind function declaration
 };
 
-void display(A obj)
+void display(A obj) // freind function initialization (return_type fun_name(Class_name obj))
 {
     cout << "Value of x : " << obj.x << endl;
 }
@@ -17,7 +17,7 @@ void display(A obj)
 int main()
 {
     A a1;
-    display(a1);
+    display(a1); // assign a permanent object as an argument
 
     return 0;
 }
